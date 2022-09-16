8aa3eb8b5dda7c81253731f0d7e9312bb6e4fe62
angeluray <aeucampos@gmail.com>
2022-09-16 01:27:56 -0400

    Add components for the respective sections

---
+++ src/components/form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => (
+  <form>
+    <input type="text" placeholder="Book title" />
+    <input type="text" placeholder="Author" />
+    <input type="submit" value="Submit" />
+  </form>
+);
+
+export default Form;
