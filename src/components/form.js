8fce69dae6066d4926abcea1428c6b59452426a0
angeluray <aeucampos@gmail.com>
2022-09-20 22:29:34 -0400

    Add new book implemented

--- src/components/form.js
+++ src/components/form.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { addMyBook } from '../redux/books/books';
 
-const Form = () => (
-  <form>
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Author" />
-    <input type="submit" value="Submit" />
-  </form>
-);
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  const submitBook = (infobook) => {
+    dispatch(addMyBook({ ...infobook, id: uuidv4() }));
+  };
+  return (
+    <form onSubmit={handleSubmit(submitBook)}>
+      <input type="text" {...register('title')} placeholder="Book title" />
+      <input type="text" {...register('author')} placeholder="Author" />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
 
 export default Form;
