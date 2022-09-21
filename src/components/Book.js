097c6d246ef494bbf43f438a17109ab9195833d0
angeluray <aeucampos@gmail.com>
2022-09-20 22:30:20 -0400

    Remove book implemented

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeMyBook } from '../redux/books/books';
 
 const Book = (props) => {
   const { book } = props;
   const { title, author } = book;
+  const dispatch = useDispatch();
+
+  const removeBook = () => {
+    dispatch(removeMyBook(book.id));
+  };
 
   return (
     <li className="bookCard">
       <h2>{title}</h2>
       <h3>{author}</h3>
       <button type="button">Comments</button>
-      <button type="button">Remove</button>
+      <button type="button" onClick={removeBook}>Remove</button>
       <button type="button">Edit</button>
     </li>
   );
