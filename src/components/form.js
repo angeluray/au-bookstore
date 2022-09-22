95673aad55091c371a99c2e08c50b05bf383b272
angeluray <aeucampos@gmail.com>
2022-09-22 18:03:26 -0400

    Add dependencies, remove hardcode and update files

--- src/components/form.js
+++ src/components/form.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { addMyBook } from '../redux/books/books';
+import { addBookFetch } from '../redux/books/books';
 
 const Form = () => {
   const { register, handleSubmit, reset } = useForm();
   const dispatch = useDispatch();
   const submitBook = (infobook) => {
-    dispatch(addMyBook({ ...infobook, id: uuidv4() }));
+    dispatch(addBookFetch({ ...infobook, item_id: uuidv4() }));
+    /* console.log(dispatch(addBookFetch({ ...infobook, item_id: uuidv4() }))); */
     reset();
   };
   return (
     <form onSubmit={handleSubmit(submitBook)}>
       <input type="text" {...register('title', { required: true })} placeholder="Book title" />
       <input type="text" {...register('author', { required: true })} placeholder="Author" />
+      <input type="text" {...register('category', { required: true })} placeholder="Category" />
       <input type="submit" value="Submit" />
     </form>
   );
